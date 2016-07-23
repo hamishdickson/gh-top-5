bf6c1aef8a1c17f33fd95ab18e5879f1b14d501a
hamishdickson <hamish.dickson@gmail.com>
2016-07-23 18:13:38 +0100

    fix ports

--- src/main/scala/com/github/hamishdickson/ghTopFive/GhTopFive.scala
+++ src/main/scala/com/github/hamishdickson/ghTopFive/GhTopFive.scala
@@ -6,8 +6,6 @@ import org.http4s.server.blaze._
 import org.http4s.argonaut._
 import _root_.argonaut._, Argonaut._
 
-import scala.util.Properties
-
 import scalaz._
 
 case class GhTopFive(host: String, port: Int) {
@@ -27,7 +25,7 @@ case class GhTopFive(host: String, port: Int) {
 
 object GhTopFive {
   val ip =   "0.0.0.0"
-  val port = Properties.envOrElse("HTTP_PORT", "5000").toInt
+  val port = sys.env("PORT").toInt
 
   def main(args: Array[String]): Unit = {
     println(s"Starting REST server on port: $port and ip: $ip")
